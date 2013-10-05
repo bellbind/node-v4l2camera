eb9fb48e1a2d247f693be9bd4613159af6c50820
bellbind <bellbind@gmail.com>
2013-10-05 18:47:04 +0900

    [update] use int arithmetic for convert yuyv to rgb

--- capture.cc
+++ capture.cc
@@ -157,22 +157,27 @@ int camera_capture(camera_t* camera)
 }
 
 
+static int minmax(int min, int v, int max)
+{
+  return (v < min) ? min : (max < v) ? max : v;
+}
+
 uint8_t* yuyv2rgb(uint8_t* yuyv, uint32_t width, uint32_t height)
 {
   uint8_t* rgb = (uint8_t*) calloc(width * height * 3, sizeof (uint8_t));
   for (size_t i = 0; i < height; i++) {
     for (size_t j = 0; j < width; j += 2) {
       size_t index = i * width + j;
-      int y0 = yuyv[index * 2 + 0];
+      int y0 = yuyv[index * 2 + 0] << 8;
       int u = yuyv[index * 2 + 1] - 128;
-      int y1 = yuyv[index * 2 + 2];
+      int y1 = yuyv[index * 2 + 2] << 8;
       int v = yuyv[index * 2 + 3] - 128;
-      rgb[index * 3 + 0] = y0 + 1.370705 * v;
-      rgb[index * 3 + 1] = y0 + 0.698001 * v - 0.337633 * u;
-      rgb[index * 3 + 2] = y0 + 1.732446 * u;
-      rgb[index * 3 + 3] = y1 + 1.370705 * v;
-      rgb[index * 3 + 4] = y1 + 0.698001 * v - 0.337633 * u;
-      rgb[index * 3 + 5] = y1 + 1.732446 * u;
+      rgb[index * 3 + 0] = minmax(0, (y0 + 359 * v) >> 8, 255);
+      rgb[index * 3 + 1] = minmax(0, (y0 + 88 * v - 183 * u) >> 8, 255);
+      rgb[index * 3 + 2] = minmax(0, (y0 + 454 * u) >> 8, 255);
+      rgb[index * 3 + 3] = minmax(0, (y1 + 359 * v) >> 8, 255);
+      rgb[index * 3 + 4] = minmax(0, (y1 + 88 * v - 183 * u) >> 8, 255);
+      rgb[index * 3 + 5] = minmax(0, (y1 + 454 * u) >> 8, 255);
     }
   }
   return rgb;
