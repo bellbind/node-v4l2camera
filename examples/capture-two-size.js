306e9802b22c9bc7f7b3a07dc06b57b24084ef33
bellbind <bellbind@gmail.com>
2013-10-13 20:24:41 +0900

    [fix] use smaller size in example

--- examples/capture-two-size.js
+++ examples/capture-two-size.js
@@ -14,28 +14,35 @@ var times = function (n, async, cont) {
 };
 
 var writePng = function (cam, filename) {
+    console.log("write " + filename + 
+                " (" + cam.width + "," + cam.height + ")");
     var rgb = cam.toRGB();
     var img = new png.Png(Buffer(rgb), cam.width, cam.height, "rgb");
     img.encode(function (buf) {
         fs.writeFileSync(filename, buf);
     });
-}
+};
 
 
 var cam = new v4l2camera.Camera("/dev/video0")
+console.log("config");
 cam.config({width: 352, height: 288});
+console.log("start");
 cam.start();
 times(6, cam.capture.bind(cam), function () {
     writePng(cam, "result1.png");
     console.log("stop");
     cam.stop(function () {
-        console.log("config");
-        cam.config({width: 640, height: 480});
+        console.log("reconfig");
+        cam.config({width: 160, height: 120});
         console.log("restart");
         cam.start();
-        cam.capture(function () {
+        times(6, cam.capture.bind(cam), function w() {
             writePng(cam, "result2.png");
-            cam.stop();
+            console.log("restop");
+            cam.stop(function () {
+                cam = null;
+            });
         });
     });
 });
