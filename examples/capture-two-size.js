c7db4c44f7cd047b1dbe8ccf6b8695b0dd5a9346
bellbind <bellbind@gmail.com>
2015-11-01 19:07:59 +0900

    [refile] rewrite examples/capture-two-size.js with es6 features

--- examples/capture-two-size.js
+++ examples/capture-two-size.js
@@ -1,48 +1,56 @@
+"use strict";
 
-var main = function () {
-    var v4l2camera = require("../");
-
-    var cam = new v4l2camera.Camera("/dev/video0");
+const main = () => flow(function* () {
+    const v4l2camera = require("../");
+    
+    const cam = new v4l2camera.Camera("/dev/video0");
     if (cam.configGet().formatName !== "YUYV") {
         console.log("YUYV camera required");
         process.exit(1);
     }
+
+    // capturing a 352x288 frame
     console.log("config");
     cam.configSet({width: 352, height: 288});
     console.log("start");
     cam.start();
-    times(6, cam.capture.bind(cam), function () {
-        saveAsPng(cam.toRGB(), cam.width, cam.height, "result1.png");
-        console.log("stop");
-        cam.stop(function () {
-            console.log("reconfig");
-            cam.configSet({width: 160, height: 120});
-            console.log("restart");
-            cam.start();
-            times(6, cam.capture.bind(cam), function () {
-                saveAsPng(cam.toRGB(), cam.width, cam.height, "result2.png");
-                console.log("restop");
-                cam.stop(function () {
-                    cam = null;
-                });
-            });
-        });
-    });
     console.log("w: " + cam.width + " h: " + cam.height);
-};
+    for (let i = 0; i < 6; i++) yield new Promise(r => cam.capture(r));
+    console.log("save frame result1.png");
+    saveAsPng(cam.toRGB(), cam.width, cam.height, "result1.png");
+    console.log("stop");
+    yield new Promise(r => cam.stop(r));
 
-var times = function (n, async, cont) {
-    return async(function rec(r) {return --n == 0 ? cont(r) : async(rec);});
-};
-var saveAsPng = function (rgb, width, height, filename) {
-    var fs = require("fs");
-    var pngjs = require("pngjs");
+    // capturing a 160x120 frame
+    console.log("reconfig");
+    cam.configSet({width: 160, height: 120});
+    console.log("restart");
+    cam.start();
+    for (let i = 0; i < 6; i++) yield new Promise(r => cam.capture(r));
+    console.log("save frame result2.png");
+    saveAsPng(cam.toRGB(), cam.width, cam.height, "result2.png");
+    console.log("quit");
+    yield new Promise(r => cam.stop(r));
+}).then(() => console.log("finished")).catch(err => console.log(err));
 
-    var png = new pngjs.PNG({
+
+// es7 `async` like function
+const flow = (gfunc) => new Promise((f, r) => {
+    const g = gfunc(), n = g.next.bind(g), t = g.throw.bind(g);
+    const subflows = item => item.done ? f(item.value) :
+          Promise.resolve(item.value).then(n, t).then(subflows, r);
+    subflows(n());
+});
+
+const saveAsPng = (rgb, width, height, filename) => {
+    const fs = require("fs");
+    const pngjs = require("pngjs");
+
+    const png = new pngjs.PNG({
         width: width, height: height, deflateLevel: 1, deflateStrategy: 1,
     });
-    var size = width * height;
-    for (var i = 0; i < size; i++) {
+    const size = width * height;
+    for (let i = 0; i < size; i++) {
         png.data[i * 4 + 0] = rgb[i * 3 + 0];
         png.data[i * 4 + 1] = rgb[i * 3 + 1];
         png.data[i * 4 + 2] = rgb[i * 3 + 2];
