b9f0d62c3538548ce92a493e2e1dd117b01f03df
bellbind <bellbind@gmail.com>
2015-11-01 19:31:33 +0900

    [fix] more es6

--- examples/capture-two-size.js
+++ examples/capture-two-size.js
@@ -10,31 +10,24 @@ const main = () => flow(function* () {
     }
 
     // capturing a 352x288 frame
-    console.log("config");
     cam.configSet({width: 352, height: 288});
-    console.log("start");
     cam.start();
-    console.log("w: " + cam.width + " h: " + cam.height);
     for (let i = 0; i < 6; i++) yield new Promise(r => cam.capture(r));
-    console.log("save frame result1.png");
     saveAsPng(cam.toRGB(), cam.width, cam.height, "result1.png");
-    console.log("stop");
+    console.log(`save (${cam.width}, ${cam.height}) as result1.png`);
     yield new Promise(r => cam.stop(r));
 
     // capturing a 160x120 frame
-    console.log("reconfig");
     cam.configSet({width: 160, height: 120});
-    console.log("restart");
     cam.start();
     for (let i = 0; i < 6; i++) yield new Promise(r => cam.capture(r));
-    console.log("save frame result2.png");
     saveAsPng(cam.toRGB(), cam.width, cam.height, "result2.png");
-    console.log("quit");
+    console.log(`save (${cam.width}, ${cam.height}) as result2.png`);
     yield new Promise(r => cam.stop(r));
 }).then(() => console.log("finished")).catch(err => console.log(err));
 
 
-// es7 `async` like function
+// Tiny es7 `async` like function
 const flow = (gfunc) => new Promise((f, r) => {
     const g = gfunc(), n = g.next.bind(g), t = g.throw.bind(g);
     const subflows = item => item.done ? f(item.value) :
@@ -47,7 +40,7 @@ const saveAsPng = (rgb, width, height, filename) => {
     const pngjs = require("pngjs");
 
     const png = new pngjs.PNG({
-        width: width, height: height, deflateLevel: 1, deflateStrategy: 1,
+        width, height, deflateLevel: 1, deflateStrategy: 1,
     });
     const size = width * height;
     for (let i = 0; i < size; i++) {
